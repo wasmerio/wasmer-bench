a3d7bc694a243f8a8002fadadec081693a8a7a54
Brandon Fish <brandon.j.fish@gmail.com>
2019-03-17 14:07:42 -0500

    Try to send data to metrics service

---
+++ send_metrics.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import csv
+import os
+
+BENCHMARK_OUT_DIR = "target/criterion"
+BENCHMARKS = ["small compile benchmark",
+              "large compile benchmark", "fib 30", "sha1", "sum 1, 2", "nbody"]
+BACKENDS = ["clif", "native", "llvm"]
+
+METRICS_API_KEY = os.environ['METRICS_API_KEY']
+METRICS_HOST = os.environ['METRICS_HOST']
+
+
+def main():
+    print("Sending benchmarks to metrics API...")
+    for benchmark in BENCHMARKS:
+        for backend in BACKENDS:
+            report_metric(benchmark, backend)
+
+
+def report_metric(benchmark, backend):
+    if backend == "native" and "compile" in benchmark:
+        return
+    avg_nanos = get_average_nanos(benchmark, backend)
+    tags = {'backend': backend}
+    send_metric(benchmark, avg_nanos, tags)
+
+
+def get_average_nanos(benchmark, backend):
+    filename = BENCHMARK_OUT_DIR + "/" + benchmark + "/" + backend + "/new/raw.csv"
+    total_nanos = 0.0
+    count = 0
+    with open(filename) as csvdatafile:
+        reader = csv.DictReader(csvdatafile)
+        for row in reader:
+            total_nanos += float(row['sample_time_nanos'])
+            count += int(row['iteration_count'])
+    return total_nanos / count
+
+
+def send_metric(name, value, tags):
+    print("Sending metric:")
+    print(name)
+#     print(value)
+#     print(tags)
+    metric = get_metric_formatted(name, value, tags)
+    import socket
+    conn = socket.create_connection(
+        (METRICS_HOST, 2003))
+    conn.send(metric.encode())
+    conn.close()
+    print(metric)
+
+
+def get_metric_formatted(name, value, tags):
+    metric = METRICS_API_KEY + ".benchmark." + \
+        name.replace(" ", "_").replace(",", "_") + "_avg_nanos"
+    tags_str = []
+    metric = metric + ";"
+    for k, v in tags.items():
+        tags_str.append(k + "=" + v)
+    metric = metric + ";".join(tags_str)
+    metric = metric + " " + str(value) + "\n"
+    return metric
+
+
+if __name__ == '__main__':
+    main()
